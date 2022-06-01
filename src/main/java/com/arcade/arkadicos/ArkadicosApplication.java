e6ae4ae8b24200d67d57ee4a316f52f7ec3e8b3a
Anitameri <la_petite_de_la_famille@hotmail.com>
2022-06-01 11:08:55 +0200

    first commit

---
+++ src/main/java/com/arcade/arkadicos/ArkadicosApplication.java
@@ -0,0 +1,13 @@
+package com.arcade.arkadicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkadicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkadicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcade/arkadicos/ArkadicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.arkadicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkadicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
