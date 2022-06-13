9a0f485dad900c0d8a54d3510389c58499509db0
UndefinedBehaviourAndIllFormed <sardevcpp@gmail.com>
2022-06-13 11:42:04 +0200

    implemented authentication entry point

--- src/main/java/com/arcade/arkadicos/security/JwtAuthentication.java
+++ src/main/java/com/arcade/arkadicos/security/JwtAuthentication.java
@@ -1,19 +1,26 @@
 package com.arcade.arkadicos.security;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
-@RestController
-@CrossOrigin(origins = "http://localhost:4200")
-public class JwtAuthentication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Component
+public class JwtAuthentication implements AuthenticationEntryPoint, Serializable
 {
-    private AuthenticationManager auth;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-    public JwtAuthentication(AuthenticationManager auth)
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException
     {
-        this.auth = auth;
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
-
-
 }
